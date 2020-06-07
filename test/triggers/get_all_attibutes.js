16e1f18735c4e5f6a11dff3d6f3450125a296644
Chandan Kumar <chandan.kumar9503@gmail.com>
2020-06-07 11:07:08 +0530

    test files

---
+++ test/triggers/get_all_attibutes.js
@@ -0,0 +1,32 @@
+require('should');
+
+const zapier = require('zapier-platform-core');
+
+const App = require('../../index');
+const appTester = zapier.createAppTester(App);
+
+describe('Trigger - get_all_attibutes', () => {
+  zapier.tools.env.inject();
+
+  it('should get an array', async () => {
+    const bundle = {
+      authData: {
+        host_address: process.env.HOST_ADDRESS,
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        oauth_consumer_key: process.env.OAUTH_CONSUMER_KEY,
+        oauth_consumer_secret: process.env.OAUTH_CONSUMER_SECRET,
+        oauth_token: process.env.OAUTH_TOKEN,
+        oauth_token_secret: process.env.OAUTH_TOKEN_SECRET,
+      },
+
+      inputData: {},
+    };
+
+    const results = await appTester(
+      App.triggers['get_all_attibutes'].operation.perform,
+      bundle
+    );
+    results.should.be.an.Array();
+  });
+});
